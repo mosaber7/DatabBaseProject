62b979f6f6b08dbe3289f8943bc862b9152b33c1
Jawad alhamadah <moon2950@hotmail.com>
2019-12-07 15:18:30 -0600

    Jawad's commit

---
+++ WindowsFormsApp1/DatabaseData/DatabaseData/DataDelegates/Query/Orders/GetShiftsDelegate.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess;
+using DatabaseData.Models;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System;
+
+namespace DatabaseData.DataDelegates.Query
+{
+    internal class GetShiftsDelegate : DataReaderDelegate<IReadOnlyList<(Waiter, Shift)>>
+    {
+        public GetShiftsDelegate()
+            : base("Restaurant.GetShifts")
+        {
+        }
+
+        public override void PrepareCommand(SqlCommand command)
+        {
+            base.PrepareCommand(command);
+        }
+       
+        public override IReadOnlyList<(Waiter, Shift)> Translate(SqlCommand command, IDataRowReader reader)
+        {
+
+            List<(Waiter, Shift)> Shifts= new List<(Waiter, Shift)>();
+            while (reader.Read())
+            {
+                Shifts.Add((
+                    new Waiter(reader.GetString("FirstName"), reader.GetString("LastName")),
+                    new Shift(Convert.ToDateTime(reader.GetString("ClockIn")),
+                    Convert.ToDateTime(reader.GetString("ClockOut")))))
+                            
+                       ;
+            }
+
+            return Shifts;
+        }
+    }
+}
---
+++ WindowsFormsApp1/DatabaseData/DatabaseData/Sql/Procedures/ReadData/GetShifts.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE Restaurant.GetOrders
+	--Show current orders
+	As
+	Select O.[Status] ,O.TableID, O.OrderedOn,W.FirstName,W.LastName
+	FROM Restaurant.Orders O inner join Restaurant.Waiters W on O.WaiterID=W.WaiterID
+	Where Status =1
+
+	Go
